39c5e9ce9e9009202dbaf5c0d0aaf41aad26889a
Erick Peripolli <erick.peripolli@gmail.com>
2023-06-11 18:18:28 -0300

    Deletes cache on retrieval of error

--- EssentialFeed/FeedCache/LocalFeedLoader.swift
+++ EssentialFeed/FeedCache/LocalFeedLoader.swift
@@ -40,6 +40,7 @@ public final class LocalFeedLoader {
             case .found, .empty:
                 completion(.success([]))
             case let .failure(error):
+                self.store.deleteCacheFeed { _ in }
                 completion(.failure(error))
             }
         }
