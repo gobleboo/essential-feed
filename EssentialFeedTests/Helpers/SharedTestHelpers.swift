ffd4190ab485630575255a34fb0736d34a5a864d
Erick Peripolli <erick.peripolli@gmail.com>
2023-06-11 19:41:01 -0300

    Extract duplicate test helpers into a shared scope

---
+++ EssentialFeedTests/Helpers/SharedTestHelpers.swift
@@ -0,0 +1,16 @@
+//
+//  SharedTestHelpers.swift
+//  EssentialFeedTests
+//
+//  Created by Erick Peripolli on 11/06/23.
+//
+
+import Foundation
+
+func anyURL() -> URL {
+    URL(string: "http://any-url.com")!
+}
+
+func anyNSError() -> NSError {
+    NSError(domain: "any error", code: 0)
+}
