6b2ccb602b8efa4fa591c9858313eb05c7aa3def
Erick Peripolli <erick.peripolli@gmail.com>
2023-07-27 20:48:25 -0300

    Move main thread dispatch to the Presenter

--- EssentialFeediOS/FeedPresentation/FeedPresenter.swift
+++ EssentialFeediOS/FeedPresentation/FeedPresenter.swift
@@ -5,6 +5,7 @@
 //  Created by Erick Peripolli on 26/07/23.
 //
 
+import Foundation
 import EssentialFeed
 
 protocol FeedLoadingView {
@@ -23,24 +24,36 @@ final class FeedPresenter {
         self.feedView = feedView
         self.loadingView = loadingView
     }
-    
+
     static var title: String {
         return NSLocalizedString("FEED_VIEW_TITLE",
-                                 tableName: "Feed",
-                                 bundle: Bundle(for: FeedPresenter.self),
-                                 comment: "Title for the feed view")
+            tableName: "Feed",
+            bundle: Bundle(for: FeedPresenter.self),
+            comment: "Title for the feed view")
     }
 
     func didStartLoadingFeed() {
+        guard Thread.isMainThread else {
+            return DispatchQueue.main.async { [weak self] in self?.didStartLoadingFeed() }
+        }
+
         loadingView.display(FeedLoadingViewModel(isLoading: true))
     }
     
     func didFinishLoadingFeed(with feed: [FeedImage]) {
+        guard Thread.isMainThread else {
+            return DispatchQueue.main.async { [weak self] in self?.didFinishLoadingFeed(with: feed) }
+        }
+
         feedView.display(FeedViewModel(feed: feed))
         loadingView.display(FeedLoadingViewModel(isLoading: false))
     }
     
     func didFinishLoadingFeed(with error: Error) {
+        guard Thread.isMainThread else {
+            return DispatchQueue.main.async { [weak self] in self?.didFinishLoadingFeed(with: error) }
+        }
+
         loadingView.display(FeedLoadingViewModel(isLoading: false))
     }
 }
