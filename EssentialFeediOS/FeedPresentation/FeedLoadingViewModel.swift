a6ef96b4669523d69d585dcfd40954a91b1f9027
Erick Peripolli <erick.peripolli@gmail.com>
2023-07-26 23:23:03 -0300

    Move ViewModels to separate files

---
+++ EssentialFeediOS/FeedPresentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Erick Peripolli on 26/07/23.
+//
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeediOS/FeedPresentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Erick Peripolli on 26/07/23.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
