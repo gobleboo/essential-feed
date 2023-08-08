b72f31e4f2db258ab4f54c7ce4de5085165cb007
Erick Peripolli <erick.peripolli@gmail.com>
2023-08-08 20:26:40 -0300

    Fix potential bug when cancelling requests in UITableView didEndDisplayingCell method - This method is invoked after calling `reloadData`, so we'd be cancelling requests in the wrong models or crash in case the model has less items than the previous model

--- EssentialApp/EssentialAppTests/Helpers/FeedUIIntegrationTests+Assertions.swift
+++ EssentialApp/EssentialAppTests/Helpers/FeedUIIntegrationTests+Assertions.swift
@@ -10,14 +10,13 @@ import EssentialFeed
 import EssentialFeediOS
 
 extension FeedUIIntegrationTests {
-    func assertThat(_ sut: FeedViewController,
-                            isRendering feed: [FeedImage],
-                            file: StaticString = #filePath,
-                            line: UInt = #line) {
+
+    func assertThat(_ sut: FeedViewController, isRendering feed: [FeedImage], file: StaticString = #file, line: UInt = #line) {
+        sut.tableView.layoutIfNeeded()
+        RunLoop.main.run(until: Date())
+        
         guard sut.numberOfRenderedFeedImageViews() == feed.count else {
-            return XCTFail("Expected \(feed.count) images, got \(sut.numberOfRenderedFeedImageViews()) instead.",
-                           file: file,
-                           line: line)
+            return XCTFail("Expected \(feed.count) images, got \(sut.numberOfRenderedFeedImageViews()) instead.", file: file, line: line)
         }
         
         feed.enumerated().forEach { index, image in
@@ -25,31 +24,19 @@ extension FeedUIIntegrationTests {
         }
     }
     
-    func assertThat(_ sut: FeedViewController,
-                            hasViewConfiguredFor image: FeedImage,
-                            at index: Int,
-                            file: StaticString = #filePath,
-                            line: UInt = #line) {
+    func assertThat(_ sut: FeedViewController, hasViewConfiguredFor image: FeedImage, at index: Int, file: StaticString = #file, line: UInt = #line) {
         let view = sut.feedImageView(at: index)
         
         guard let cell = view as? FeedImageCell else {
-            return XCTFail("Expected \(FeedImageCell.self) instance, got \(String(describing: view)) instead.", file: file, line: line)
+            return XCTFail("Expected \(FeedImageCell.self) instance, got \(String(describing: view)) instead", file: file, line: line)
         }
         
         let shouldLocationBeVisible = (image.location != nil)
-        XCTAssertEqual(cell.isShowingLocation, shouldLocationBeVisible,
-                       "Expected `isShowingLocation` to be \(shouldLocationBeVisible) for iamge view at index (\(index))",
-                       file: file,
-                       line: line)
+        XCTAssertEqual(cell.isShowingLocation, shouldLocationBeVisible, "Expected `isShowingLocation` to be \(shouldLocationBeVisible) for image view at index (\(index))", file: file, line: line)
         
-        XCTAssertEqual(cell.locationText, image.location,
-                       "Expected location text to be \(String(describing: image.location)) for image view at index (\(index))",
-                       file: file,
-                       line: line)
+        XCTAssertEqual(cell.locationText, image.location, "Expected location text to be \(String(describing: image.location)) for image  view at index (\(index))", file: file, line: line)
         
-        XCTAssertEqual(cell.descriptionText, image.description,
-                       "Expected description text to be \(String(describing: image.description)) for image view at index (\(index))",
-                       file: file,
-                       line: line)
+        XCTAssertEqual(cell.descriptionText, image.description, "Expected description text to be \(String(describing: image.description)) for image view at index (\(index)", file: file, line: line)
     }
+    
 }
