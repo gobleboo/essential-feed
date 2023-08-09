38ba46937a4ebaef152a1e0639f6ca9b89848880
Erick Peripolli <erick.peripolli@gmail.com>
2023-08-08 21:27:17 -0300

    fix scene make key and visible update for iOS 15

--- EssentialApp/EssentialAppTests/SceneDelegateTests.swift
+++ EssentialApp/EssentialAppTests/SceneDelegateTests.swift
@@ -12,14 +12,11 @@ import EssentialFeediOS
 class SceneDelegateTests: XCTestCase {
     
     func test_configureWindow_setsWindowAsKeyAndVisible() {
-        let window = UIWindow()
-        let sut = SceneDelegate()
-        sut.window = window
-
-        sut.configureWindow()
-
-        XCTAssertTrue(window.isKeyWindow, "Expected window to be the key window")
-        XCTAssertFalse(window.isHidden, "Expected window to be visible")
+      let window = UIWindowSpy()
+      let sut = SceneDelegate()
+      sut.window = window
+      sut.configureWindow()
+      XCTAssertEqual(window.makeKeyAndVisibleCallCount, 1, "Expected to make window key and visible")
     }
 
     func test_configureWindow_configuresRootViewController() {
@@ -34,6 +31,13 @@ class SceneDelegateTests: XCTestCase {
 
         XCTAssertNotNil(rootNavigation, "Expected a navigation controller as root, got \(String(describing: root)) instead")
         XCTAssertTrue(topController is FeedViewController, "Expected a feed controller as top view controller, got \(String(describing: topController)) instead")
+    }
+    
+    private class UIWindowSpy: UIWindow {
+      var makeKeyAndVisibleCallCount = 0
+      override func makeKeyAndVisible() {
+        makeKeyAndVisibleCallCount = 1
+      }
     }
 
 }
