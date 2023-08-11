9e48c59bd8c1cf6a95890b8ba4ed99d3ca6544b6
Erick Peripolli <erick.peripolli@gmail.com>
2023-08-11 19:21:43 -0300

    Optimize CoreData implementation to reduce cache misses

--- EssentialFeed/EssentialFeed/FeedCache/Infrastructure/CoreData/ManagedFeedImage.swift
+++ EssentialFeed/EssentialFeed/FeedCache/Infrastructure/CoreData/ManagedFeedImage.swift
@@ -8,7 +8,7 @@
 import CoreData
 
 @objc(ManagedFeedImage)
-final class ManagedFeedImage: NSManagedObject {
+class ManagedFeedImage: NSManagedObject {
     @NSManaged var id: UUID
     @NSManaged var imageDescription: String?
     @NSManaged var location: String?
@@ -19,9 +19,11 @@ final class ManagedFeedImage: NSManagedObject {
 
 extension ManagedFeedImage {
     static func data(with url: URL, in context: NSManagedObjectContext) throws -> Data? {
+        if let data = context.userInfo[url] as? Data { return data }
+        
         return try first(with: url, in: context)?.data
     }
-    
+
     static func first(with url: URL, in context: NSManagedObjectContext) throws -> ManagedFeedImage? {
         let request = NSFetchRequest<ManagedFeedImage>(entityName: entity().name!)
         request.predicate = NSPredicate(format: "%K = %@", argumentArray: [#keyPath(ManagedFeedImage.url), url])
@@ -29,19 +31,28 @@ extension ManagedFeedImage {
         request.fetchLimit = 1
         return try context.fetch(request).first
     }
-    
+
     static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
-        NSOrderedSet(array: localFeed.map { local in
+        let images = NSOrderedSet(array: localFeed.map { local in
             let managed = ManagedFeedImage(context: context)
             managed.id = local.id
             managed.imageDescription = local.description
             managed.location = local.location
             managed.url = local.url
+            managed.data = context.userInfo[local.url] as? Data
             return managed
         })
+        context.userInfo.removeAllObjects()
+        return images
     }
     
     var local: LocalFeedImage {
-        LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
+        return LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
+    }
+    
+    override func prepareForDeletion() {
+        super.prepareForDeletion()
+        
+        managedObjectContext?.userInfo[url] = data
     }
 }
